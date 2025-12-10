c833751f36a5ce9c41ddeb8bcf7a23301cab0f91
arxdsdd07-arch <arxdsdd07@gmail.com>
2025-12-10 20:45:39 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,7 @@
+repeat wait() until game:IsLoaded() 
+local Place = game.PlaceId
+if Place == 15601725874 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arxdsdd07-arch/script/refs/heads/main/Protected_1379384898613797.lua"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/arxdsdd07-arch/script/refs/heads/main/auv.lua"))()
+end
